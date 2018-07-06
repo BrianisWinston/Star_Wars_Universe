ba8f42070c0b2103ea4d831c96657cc63f60ff86
Winston Galas <BrianisWinston@yahoo.com>
2018-07-05 21:52:49 -0700

    Create Person and Film migration and create schema

--- db/migrate/20180706023607_create_films.rb
+++ db/migrate/20180706023607_create_films.rb
@@ -1,6 +1,21 @@
 class CreateFilms < ActiveRecord::Migration[5.2]
   def change
     create_table :films do |t|
+      t.string :title, null: false
+      t.integer :episode_id, null: false
+      t.string :opening_crawl, null: false
+      t.string :director, null: false
+      t.string :producer, null: false
+      t.date :release_date, null: false
+      t.text :species, array: true, default: []
+      t.text :starships, array: true, default: []
+      t.text :vehicles, array: true, default: []
+      t.text :characters, array: true, default: []
+      t.text :planets, array: true, default: []
+      t.string :url, null: false
+      t.string :created, null: false
+      t.string :edited, null: false
+
 
       t.timestamps
     end
--- db/migrate/20180706023617_create_people.rb
+++ db/migrate/20180706023617_create_people.rb
@@ -1,6 +1,22 @@
 class CreatePeople < ActiveRecord::Migration[5.2]
   def change
     create_table :people do |t|
+      t.string :name, null: false
+      t.string :birth_year, null: false
+      t.string :eye_color, null: false
+      t.string :gender, null: false
+      t.string :hair_color, null: false
+      t.string :height, null: false
+      t.string :mass, null: false
+      t.string :skin_color, null: false
+      t.string :homeworld, null: false
+      t.text :films, array: true, default: []
+      t.text :species, array: true, default: []
+      t.text :starships, array: true, default: []
+      t.text :vehicles, array: true, default: []
+      t.string :url, null: false
+      t.string :created, null: false
+      t.string :edited, null: false
 
       t.timestamps
     end
