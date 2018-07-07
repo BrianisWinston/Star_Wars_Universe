61d637401d417e54a1425f7c7270743a08e29706
Winston Galas <BrianisWinston@yahoo.com>
2018-07-06 18:37:02 -0700

    Create model level validations

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,2 +1,6 @@
 class Film < ApplicationRecord
+  validates :title, :episode_id, :opening_crawl, :director,
+  :producer, :release_date, :species, :starships, :vehicles,
+  :characters, :planets, :url, :created, :edited, :created_at,
+  :updated_at, presence: true
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,6 @@
 class Person < ApplicationRecord
+  validates :name, :birth_year, :eye_color, :gender,
+  :hair_color, :height, :mass, :skin_color, :homeworld,
+  :films, :species, :starships, :vehicles, :url, :created,
+  :edited, :created_at, :updated_at, presence: true
 end
