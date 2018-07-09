847c185441b606c057a8145e6897cf6833ad00f9
Winston Galas <BrianisWinston@yahoo.com>
2018-07-09 16:44:29 -0700

    Update webpack

---
+++ frontend/components/home/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class AllFilms extends React.Component {
+  return () {
+    <div>
+      <h1>whas good bruh bruh</h1>
+    </div>
+  }
+}
+
+export default AllFilms;
--- frontend/components/home/home_container.jsx
+++ frontend/components/home/home_container.jsx
@@ -1,1 +1,14 @@
-return  <h1>hello bruh</h1>
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchFilms } from '../../actions/film_actions';
+import Home from './home';
+
+const mapStateToProps = (state) => ({
+  photos: Object.values(state.films)
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchFilms: () => dispatch(fetchFilms()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
