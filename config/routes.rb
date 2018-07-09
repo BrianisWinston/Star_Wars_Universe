20740c1b847961599040ef5516f57cf5b09d40a4
Winston Galas <BrianisWinston@yahoo.com>
2018-07-08 22:51:32 -0700

    Add api namespace to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :films, only: [:new, :create, :index, :show]
-  resources :people, only: [:new, :create, :index, :show]
+  namespace :api, defaults: {format: :json} do
+    resources :films, only: [:new, :create, :index, :show]
+    resources :people, only: [:new, :create, :index, :show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
